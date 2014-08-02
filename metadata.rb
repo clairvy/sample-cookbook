da6e1bfbaf04e7cd9d6fb4a0a05ba70881f33d3e
clairvy <clairvy@gmail.com>
2014-08-02 13:20:43 +0900

    initial revision

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'sample-erlang'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures sample-erlang'
+long_description 'Installs/Configures sample-erlang'
+version          '0.1.0'
+
