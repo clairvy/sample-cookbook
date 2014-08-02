da9974e07e7c46cc09a7267721d64c67cf0c1eba
clairvy <clairvy@gmail.com>
2014-08-02 16:30:01 +0900

    append recipe to install

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+url = "http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_17.1-1~centos~6_amd64.rpm"
+filename = "/usr/local/rpms/#{File.basename url}"
+dirname = File.dirname filename
+basename = File.basename filename
+
+directory dirname do
+end
+
+remote_file filename do
+  source url
+end
+
+yum_package basename do
+  source filename
+end
